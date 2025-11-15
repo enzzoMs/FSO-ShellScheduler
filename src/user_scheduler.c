8222d4cdb7b37149bf38eff888838025dcb00703
Enzzo Morais <enzzomorais0@gmail.com>
2025-11-15 16:37:29 -0300

    Lógica do escalonamento de processos

--- src/user_scheduler.c
+++ src/user_scheduler.c
@@ -15,12 +15,17 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
 #include "utils.h"
 
+#define QUANTUM_SECONDS 4
 #define HIGHEST_PRIORITY 1
 
 struct process_t {
   char program_name[100];
+  int pid;
   int priority;
   struct process_t *next;
   struct process_t *previous;
@@ -48,6 +53,7 @@ int num_of_queues;
 int receive_scheduling_req(int msq_id, struct scheduler_req_t *msg);
 void enqueue_process(struct scheduler_req_t *msg);
 int get_scheduler_msg_queue();
+void execute_scheduling();
 
 int main(int argc, char *argv[]) {
   int shell_sem_id = get_shell_semaphore();
@@ -60,12 +66,15 @@ int main(int argc, char *argv[]) {
     printf(
       "\033[1;31mERRO!!!\033[0m Argumento inválido para 'user_scheduler'. Só é possível criar 2 ou 3 filas.\n"
     );
-    up_sem(shell_sem_id);
+    up_sem(shell_sem_id); // Libera o shell_sched
     exit(1);
   }
 
+  signal(SIGALRM, execute_scheduling);
+  alarm(QUANTUM_SECONDS); // O escalonador será acordado em QUANTUM_SECONDS segundos
+
   printf("\nProcesso 'user_scheduler' inicializado com %d filas.\n", num_of_queues);
-  up_sem(shell_sem_id);
+  up_sem(shell_sem_id); // Libera o shell_sched
 
   int msq_id = get_scheduler_msg_queue();
 
@@ -83,7 +92,12 @@ int main(int argc, char *argv[]) {
 int receive_scheduling_req(int msq_id, struct scheduler_req_t *msg) {
   int res = msgrcv(msq_id, msg, SCHED_MSG_SIZE, 1, 0);
   if (res == -1) {
-    printf("\nError: Unable to receive scheduling request!\n");
+    // O escalonador pode receber a interrupção SIGALRM enquanto ele estiver esperando
+    // pela mensagem. Nesse caso, a fila de mensagens vai retornar um erro EINTR (de interrupção),
+    // porém, como esse comportamento é esperado, nós ignoramos o erro nesses casos.
+    if (errno != EINTR) {
+      printf("\nError: Unable to receive scheduling request!\n");
+    }
     return 0;
   }
   return 1;
@@ -91,6 +105,10 @@ int receive_scheduling_req(int msq_id, struct scheduler_req_t *msg) {
 
 // Cria e enfilera um processo na sua fila de prioridade.
 void enqueue_process(struct scheduler_req_t *msg) {
+  // Reseta o relógio para impedir que o escalonador seja interrompido
+  // enquanto ele faz a atualização das filas
+  int remaining_quantum = alarm(0);
+
   char *program_name = strtok(msg->mtext, " ");
   int priority = strtol(strtok(NULL, " "), NULL, 10);
 
@@ -99,9 +117,18 @@ void enqueue_process(struct scheduler_req_t *msg) {
     return;
   }
 
+  int process_pid = fork();
+  if (process_pid == 0) {
+    raise(SIGSTOP);  // O novo processo começa parado
+    execl(program_name, program_name, NULL);
+    printf("\nError: Can't execute program '%s'!\n", program_name);
+    exit(1);
+  }
+
   struct process_t *new_process = malloc(sizeof(struct process_t));
   strcpy(new_process->program_name, program_name);
   new_process->priority = priority;
+  new_process->pid = process_pid;
   new_process->next = NULL;
   new_process->previous = NULL;
 
@@ -117,6 +144,35 @@ void enqueue_process(struct scheduler_req_t *msg) {
     temp->next = new_process;
     new_process->previous = temp;
   }
+
+  if (current_process != NULL && current_process->priority < priority) {
+    // Se o novo processo tiver uma prioridade maior, então realizamos
+    // o escalonamento novamente para que ele seja escolhido
+    execute_scheduling();
+  } else {
+    alarm(remaining_quantum);
+  }
+}
+
+// Executa o escalonamento dos processos, escolhendo o processo de
+// maior prioridade para ser executado.
+void execute_scheduling() {
+  alarm(0);
+  if (current_process != NULL) {
+    kill(current_process->pid, SIGSTOP);
+  }
+  struct process_t *next_process = NULL;
+  for (int i = 0; i < num_of_queues; i++) {
+    if (process_queues[i] != NULL) {
+      next_process = process_queues[i];
+      break;
+    }
+  }
+  current_process = next_process;
+  if (next_process != NULL) {
+    kill(next_process->pid, SIGCONT);
+  }
+  alarm(QUANTUM_SECONDS);
 }
 
 // Cria e retorna o identificador da fila de mensagens do escalonador.
